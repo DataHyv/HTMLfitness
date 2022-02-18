53b3ac812e01d60154103914867c385e50903655
ericogalileo <erick@datahyv.com>
2022-02-18 11:04:29 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,177 @@
+    <!--- Header --->
+    <!-- <header class="text-center text-white masthead">
+        <div class="masthead-content">
+            <div class="container container-header">
+                <h2 class="text-uppercase text-center text-black masthead-up mb-0">The</h2>
+                <h1 class="text-uppercase shadow-none pulse animated masthead-heading mb-0">Extra Mile</h1>
+                <h2 class="text-uppercase masthead-down">Challenge</h2>
+                <h2 class="text-center masthead-subheading mb-0" style="color: rgb(18,14,0);"><br>Let us improve our health by taking those extra steps. May it be running, jogging,
+                    or walking, every step you make is a step closer to a better and helthier life.</h2>
+                <a class="btn btn-header text-white fw-bold btn-xl rounded-pill mt-4 header-btn" role="button" data-bss-hover-animate="pulse" href="#content1">Learn More</a>
+            </div>
+        </div>
+    </header> -->
+
+    <!--- Header --->
+    <header>
+        <!-- overlay effect on the video -->
+        <div class="overlay"></div>
+
+        <!-- The HTML5 video element that will create the background video on the header -->
+        <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+            <source src="<?php echo base_url('assets/videos/running.mp4')?>" type="video/mp4">
+        </video>
+
+        <!-- The header content -->
+        <div class="container h-100">
+            <div class="d-flex h-100 text-center align-items-center">
+                <div class="w-100 text-white">
+                    <h1 class="text-uppercase shadow-none pulse animated masthead-heading mb-0 display-1 fw-bold">Extra Mile</h1>
+                    <p class="lead mb-0">Let us improve our health by taking those extra steps. May it be running, jogging,
+                        or walking, every step you make is a step closer to a better and Helthier Life.</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--- /Header --->
+
+    <!-- ======= Featured Services Section ======= -->
+    <section id="featured-services" class="featured-services" style="background: #e0e6e6;">
+        <div class="container">
+
+            <div class="row gy-4">
+                <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
+                    <div class="service-item position-relative">
+                        <div class="icon"><i class="bi bi-list-task icon"></i></div>
+                        <h4><a href="" class="stretched-link">Lorem Ipsum</a></h4>
+                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                    </div>
+                </div><!-- End Service Item -->
+
+                <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out" data-aos-delay="200">
+                    <div class="service-item position-relative">
+                        <div class="icon"><i class="bi bi-activity icon"></i></div>
+                        <h4><a href="" class="stretched-link">Sed ut perspici</a></h4>
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                    </div>
+                </div><!-- End Service Item -->
+
+                <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out" data-aos-delay="400">
+                    <div class="service-item position-relative">
+                        <div class="icon"><i class="bi bi-calendar4-week icon"></i></div>
+                        <h4><a href="" class="stretched-link">Magni Dolores</a></h4>
+                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+                    </div>
+                </div><!-- End Service Item -->
+
+                <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out" data-aos-delay="600">
+                    <div class="service-item position-relative">
+                        <div class="icon"><i class="bi bi-heart-fill icon"></i></div>
+                        <h4><a href="" class="stretched-link">Nemo Enim</a></h4>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                    </div>
+                </div><!-- End Service Item -->
+
+            </div>
+
+        </div>
+    </section>
+    <!-- End Featured Services Section -->
+
+
+    <!-- ======= Details Section ======= -->
+    <section id="details" class="details">
+        <div class="container">
+
+            <div class="row content">
+                <div class="col-md-4" data-aos="fade-right">
+                    <img src="assets/img/undraw_task_list.svg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-8 pt-4" data-aos="fade-up">
+                    <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+                    <p class="fst-italic">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </p>
+                    <ul>
+                        <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                        <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                        <li><i class="bi bi-check"></i> Iure at voluptas aspernatur dignissimos doloribus repudiandae.</li>
+                        <li><i class="bi bi-check"></i> Est ipsa assumenda id facilis nesciunt placeat sed doloribus praesentium.</li>
+                    </ul>
+                    <p>
+                        Voluptas nisi in quia excepturi nihil voluptas nam et ut. Expedita omnis eum consequatur non. Sed in asperiores aut repellendus. Error quisquam ab maiores.
+                        Quibusdam sit in officia
+                    </p>
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
+                    <img src="assets/img/undraw_working_out.svg " class="img-fluid" alt="">
+                </div>
+                <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
+                    <h3>Corporis temporibus maiores provident</h3>
+                    <p class="fst-italic">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </p>
+                    <p>
+                        Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum
+                    </p>
+                    <p>
+                        Inventore id enim dolor dicta qui et magni molestiae. Mollitia optio officia illum ut cupiditate eos autem. Soluta dolorum repellendus repellat amet autem
+                        rerum illum in. Quibusdam occaecati est nisi esse. Saepe aut dignissimos distinctio id enim.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-4" data-aos="fade-right">
+                    <img src="assets/img/undraw_activity_tracker.svg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-8 pt-5" data-aos="fade-up">
+                    <h3>Sunt consequatur ad ut est nulla consectetur reiciendis animi voluptas</h3>
+                    <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>
+                    <ul>
+                        <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                        <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                        <li><i class="bi bi-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
+                    </ul>
+                    <p>
+                        Qui consequatur temporibus. Enim et corporis sit sunt harum praesentium suscipit ut voluptatem. Et nihil magni debitis consequatur est.
+                    </p>
+                    <p>
+                        Suscipit enim et. Ut optio esse quidem quam reiciendis esse odit excepturi. Vel dolores rerum soluta explicabo vel fugiat eum non.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row content">
+                <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
+                    <img src="assets/img/undraw_complete_task.svg" class="img-fluid " alt="">
+                </div>
+                <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
+                    <h3>Quas et necessitatibus eaque impedit ipsum animi consequatur incidunt in</h3>
+                    <p class="fst-italic">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </p>
+                    <p>
+                        Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum
+                    </p>
+                    <ul>
+                        <li><i class="bi bi-check"></i> Et praesentium laboriosam architecto nam .</li>
+                        <li><i class="bi bi-check"></i> Eius et voluptate. Enim earum tempore aliquid. Nobis et sunt consequatur. Aut repellat in numquam velit quo dignissimos et.
+                        </li>
+                        <li><i class="bi bi-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Details Section -->
